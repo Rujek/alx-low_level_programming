d4891d709a0292ce48256c8b64d505de12cf52a5
Rujek <rujeko24@gmail.com>
2023-02-17 06:20:19 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,8 +19,7 @@ int main(void)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else if (n < 0)
+	else
 		printf("%d is negative\n", n);
-	printf("\n");
 	return (0);
 }
