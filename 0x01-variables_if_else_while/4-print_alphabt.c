0b1fb13d3ef6a9ec96f54b01a6e998ad730e6b91
Rujek <rujeko24@gmail.com>
2023-02-17 12:25:51 -0800

    Task 5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,13 @@ int main(void)
 	char ch = 'a'
 
 	while (ch <= 'z')
-	if (ch != 'e' && ch != 'q')
-		putchar(ch);
+	{
+	 if (ch != 'e' && ch != 'q')
+	{
+	 putchar(ch);
+	}
 	ch++;
+	}
 	putchar('\n');
 	return (0);
 }
