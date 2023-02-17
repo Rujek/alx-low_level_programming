e50fdd0e96fa099e4e696b233bf03c4f4497e5d2
Rujek <rujeko24@gmail.com>
2023-02-16 18:04:15 -0800

    Task 8

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-#include <sunistd.h>
+#include <unistd.h>
 /**
  * main - Entry point
- *
- * Return: Always 1.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	fput(" and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
