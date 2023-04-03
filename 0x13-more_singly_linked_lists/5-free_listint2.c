818aaf2cb8f07ec0900841a1574d40e1d7db1411
Rujek <rujeko24@gmail.com>
2023-04-03 08:28:37 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void free_listint2(listint_t **head)
+
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	head = NULL;
+}
