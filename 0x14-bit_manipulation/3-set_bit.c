dee189de5390bb75656eb20e077ea2cfa7f514b6
Rujek <rujeko24@gmail.com>
2023-05-05 05:26:50 -0700

    Set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,9 +11,9 @@ int set_bit(unsigned long int *n, unsigned int
 index)
 
 {
-	if (index > 63)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n = ((1UL << index) | *n);
-	return (-1);
+	*n ^= (1 << index);
+	return (1);
 }
